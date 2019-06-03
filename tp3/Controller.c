d0813609f9997eafe60197996dc50c62895b18ed
Cristian olv <47321357+nimref149@users.noreply.github.com>
2019-06-03 12:19:21 -0300

    Add files via upload

---
+++ tp3/Controller.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromText(char* path, LinkedList* pArrayListEmployee)
+{
+
+
+
+
+
+
+
+
+
+/*
+    char id[500],nombre[500],horasTrabajas[500],sueldo[500];
+
+    LinkedList* lista[1001];
+
+    int cont=0;
+
+    FILE * pFile;
+
+    pFile = fopen (path, "a+");
+
+    fscanf(pFile, "%[^,],%[^,],%[^,],%[^,],%[^\n]\n",id,nombre,horasTrabajadas,sueldo);
+
+    while(!feof(pFile))
+    {
+
+        fscanf(pFile, "%[^,],%[^,],%[^,],%[^\n]\n",id, nombre, horasTrabajas,sueldo);
+
+        producto = (eProducto*)malloc(sizeof(eProducto));
+        producto->codigo = atoi(codigo);
+        producto->categoria = atoi(categoria);
+        strcpy(producto->descripcion, descripcion);
+        producto->precio = atof(precio);
+
+        //  printf("TEXTO : %s - %s - %s - %s\n", codigo, descripcion, categoria, precio);
+        lista[cont] = producto;
+        //printf("STRUCT: %d - %s - %d - %.2f\n", producto.codigo, producto.descripcion, producto.categoria, producto.precio);
+        cont++;
+    }
+    */
+    return 1;
+
+}
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Alta de empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_addEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Modificar datos de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_editEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Baja de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_removeEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Listar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_ListEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Ordenar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_sortEmployee(LinkedList* pArrayListEmployee)
+{
+
+
+
+
+
+    return 1;
+}
+
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsText(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsBinary(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
---
+++ tp3/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+     1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).
+     2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).
+     3. Alta de empleado
+     4. Modificar datos de empleado
+     5. Baja de empleado
+     6. Listar empleados
+     7. Ordenar empleados
+     8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
+     9. Guardar los datos de los empleados en el archivo data.csv (modo binario).
+    10. Salir
+*****************************************************/
+int menuOpciones(char texto[]);
+
+int main()
+{
+    int option = 0;
+    LinkedList* listaEmpleados = ll_newLinkedList();
+    do{
+        option=menuOpciones("Menu\n1-Cargar los datos de los empleados desde el archivo data.csv\n2.Cargar los datos de los empleados desde el archivo data.csv (modo binario)\n3.Alta de empleado\n4.Modificar datos de empleado\n5.Baja de empleado\n6.Listar empleados\n7.Ordenar empleados\n8.Guardar los datos de los empleados en el archivo data.csv (modo texto).\n9.Guardar los datos de los empleados en el archivo data.csv (modo binario)\n10-Salir\n");
+
+        switch(option)
+        {
+        case 1:
+            controller_loadFromText("data.csv",listaEmpleados);
+
+            break;
+        case 2:
+            controller_loadFromBinary("data.csv",listaEmpleados);
+            break;
+        case 3:
+            controller_addEmployee(listaEmpleados);
+            break;
+        case 4:
+            controller_editEmployee(listaEmpleados);
+            break;
+        case 5:
+            controller_removeEmployee(listaEmpleados);
+            break;
+        case 6:
+            controller_ListEmployee(listaEmpleados);
+            break;
+        case 7:
+            controller_sortEmployee(listaEmpleados);
+            break;
+        case 8:
+            controller_saveAsText("data.csv",listaEmpleados);
+            break;
+        case 9:
+            controller_saveAsBinary("data.csv",listaEmpleados);
+            break;
+        case 10:
+            printf("esta saliendo de programa");
+            break;
+        default:
+            printf("La opcion ingresada no es correcta");
+            break;
+        }
+    }while(option != 10);
+    return 0;
+}
+int menuOpciones(char texto[]){
+int numero;
+printf(texto);
+scanf("%d",&numero);
+return numero;
+}
+/*
+int retornar(int *num,int *num2)
+{
+    int numero;
+    char palabra[];
+    int num2;
+    int registro;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
